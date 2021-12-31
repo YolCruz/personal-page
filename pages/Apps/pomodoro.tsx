5b5a6cc29d5cbec3ca8cf6632f3a65060c2df8d5
YolticCruzTello <yolcruztello@gmail.com>
2021-12-31 00:39:12 -0600

    Changed text size

--- pages/Apps/pomodoro.tsx
+++ pages/Apps/pomodoro.tsx
@@ -296,8 +296,12 @@ export default function Pomodoro() {
           </div>
         </div>
         <div className="w-11/12 max-w-3xl flex flex-col mt-10 gap-4 bg-sky-200 dark:bg-black dark:bg-opacity-70 mx-auto p-6 xl:p-10 rounded-xl text-left font-open-sans duration-300 ease-out">
-          <h1 className="text-3xl sm:text-4xl mb-4">What I learned doing this proyect:</h1>
-          <h2 className="text-2xl sm:text-3xl text-red-400 font-bold">React hooks</h2>
+          <h1 className="text-3xl sm:text-4xl mb-4">
+            What I learned doing this proyect:
+          </h1>
+          <h2 className="text-2xl sm:text-3xl text-red-400 font-bold">
+            React hooks
+          </h2>
           <p className="text-xl sm:text-2xl mb-2">
             While creating the functionality of this application, I learned to
             use React hooks such as{" "}
@@ -311,12 +315,24 @@ export default function Pomodoro() {
             and{" "}
             <code className="bg-white dark:bg-gray-700 p-1 rounded-lg">
               useRef()
-            </code>.
+            </code>
+            .
           </p>
-          <h2 className="text-2xl sm:text-3xl text-purple-400 font-bold">Conditional rendering of elements</h2>
-          <p className="text-xl sm:text-2xl mb-2">There are many elements that only render when certain conditions are met. For example, starting the timer and then trying to stop it will render a screen that asks for a confirmation.</p>
-          <h2 className="text-2xl sm:text-3xl text-green-400 font-bold">Handle click events</h2>
-          <p className="text-xl sm:text-2xl">All the interactions are done with buttons that change some state when you click them.</p>
+          <h2 className="text-2xl sm:text-3xl text-purple-400 font-bold">
+            Conditional rendering of elements
+          </h2>
+          <p className="text-xl sm:text-2xl mb-2">
+            There are many elements that only render when certain conditions are
+            met. For example, starting the timer and then trying to stop it will
+            render a screen that asks for a confirmation.
+          </p>
+          <h2 className="text-2xl sm:text-3xl text-green-400 font-bold">
+            Handle click events
+          </h2>
+          <p className="text-xl sm:text-2xl">
+            All the interactions are done with buttons that change some state
+            when you click them.
+          </p>
         </div>
       </div>
     </LayoutApps>
