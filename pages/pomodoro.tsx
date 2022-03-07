1617d514af255872ad7a31e47dfc9eac99c82a66
YolticCruzTello <ticyolollin@hotmail.com>
2022-03-07 00:51:29 -0600

    Removed unused import

--- pages/pomodoro.tsx
+++ pages/pomodoro.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { AiFillHome } from "react-icons/ai";
 import { Provider } from "react-redux";
